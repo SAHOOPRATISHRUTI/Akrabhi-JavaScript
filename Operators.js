d8bbe0ed554b0032f84a5ab4e867a14becc84a29
SAHOOPRATISHRUTI <pratishrutisahoo7@gmail.com>
2023-11-11 12:07:39 +0530

    Intial Commit

---
+++ Operators.js
@@ -0,0 +1,21 @@
+//Assignment Operator
+let a = 10;
+a += 5;
+
+//JavaScript String Comparison
+let text1 = "A";
+let text2 = "B";
+let result = text1 < text2;
+
+//JavaScript String Addition
+let text3 = "John";
+let text4 = "Doe";
+let text5 = text1 + " " + text2;
+
+//Adding Strings and Numbers
+let x = 5 + 5;
+let y = "5" + 5;
+let z = "Hello" + 5;
+
+//Simple Assignment
+let s = 10;
