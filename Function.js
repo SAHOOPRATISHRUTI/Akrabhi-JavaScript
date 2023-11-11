c5c2a55d5d6594ac6d34ee2c9c1a140d4e637db5
SAHOOPRATISHRUTI <pratishrutisahoo7@gmail.com>
2023-11-11 12:19:08 +0530

    Intaial Commit

---
+++ Function.js
@@ -0,0 +1,23 @@
+function myFunction(p1, p2) 
+{
+    return p1 * p2;
+}
+
+
+let x = myFunction(4, 3);
+
+function myFunction(a, b) 
+{
+  return a * b; // Function returns the product of a and b
+}
+
+function myFunction() 
+{
+    alert("Hello World!");
+}
+myFunction();
+
+function myFunction() 
+{
+    document.getElementById("demo").innerHTML = "Hello";
+}
