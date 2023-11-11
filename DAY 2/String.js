a3c0bcd76acf686a81c082b073472e16e1830992
SAHOOPRATISHRUTI <pratishrutisahoo7@gmail.com>
2023-11-11 14:02:52 +0530

    Initial Commit

---
+++ DAY 2/String.js
@@ -0,0 +1,11 @@
+let carName1 = "Volvo XC60";  // Double quotes
+let carName2 = 'Volvo XC60';  // Single quotes
+
+let answer1 = "It's alright";
+let answer2 = "He is called 'Johnny'";
+let answer3 = 'He is called "Johnny"';
+
+
+//String Length
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let length = text.length;
