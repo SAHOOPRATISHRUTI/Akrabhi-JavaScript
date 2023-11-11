504eb21c82ba8e3bf2b97b67cff2d13e0e954df9
SAHOOPRATISHRUTI <pratishrutisahoo7@gmail.com>
2023-11-11 14:27:25 +0530

    Initial Commit

---
+++ DAY 2/NUMBERS/Numbers Methods.js
@@ -0,0 +1,26 @@
+let x = 123;
+x.toString();
+(123).toString();
+(100 + 23).toString();
+
+let y= 9.656;
+y.toExponential(2);
+y.toExponential(4);
+y.toExponential(6);
+
+let x1= 9.656;
+x1.toFixed(0);
+x1.toFixed(2);
+x1.toFixed(4);
+x1.toFixed(6);
+
+let x2 = 9.656;
+x2.toPrecision();
+x2.toPrecision(2);
+x2.toPrecision(4);
+x2.toPrecision(6);
+
+let x3= 123;
+x3.valueOf();
+(123).valueOf();
+(100 + 23).valueOf();
