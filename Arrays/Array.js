fdc101be6c044747b9e386bddb2f7961d07557e3
SAHOOPRATISHRUTI <pratishrutisahoo7@gmail.com>
2023-11-11 14:43:59 +0530

    Initial Commit

---
+++ Arrays/Array.js
@@ -0,0 +1,34 @@
+let car1 = "Saab";
+let car2 = "Volvo";
+let car3 = "BMW";
+
+//Creating an Array
+//const array_name = [item1, item2, ...]; 
+
+const cars = [];
+cars[0]= "Saab";
+cars[1]= "Volvo";
+cars[2]= "BMW";
+
+
+//JavaScript Array length
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let size = fruits.length;
+
+//const fruits = ["Banana", "Orange", "Apple", "Mango"]; //pop
+fruits.pop();
+
+//JavaScript Array toString()
+const fruit = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("demo").innerHTML = fruits.toString();
+
+
+//JavaScript Push()
+//const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.push("Kiwi");
+
+//Javascript Array Shift
+//const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.shift();
+
+
