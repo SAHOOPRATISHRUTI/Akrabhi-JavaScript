b72c1236952d3a7c42f29dc795c5938dcea61e19
SAHOOPRATISHRUTI <pratishrutisahoo7@gmail.com>
2023-11-11 14:23:23 +0530

    Initial Commit

---
+++ DAY 2/NUMBERS/Numbers.js
@@ -0,0 +1,8 @@
+let x = 3.14;    // A number with decimals
+let y = 3;       // A number without decimals
+
+let x1 = 123e5;    // 12300000
+let y1 = 123e-5;   // 0.00123
+
+let x2 = 999999999999999;   // x will be 999999999999999
+let y3 = 9999999999999999;  // y will be 10000000000000000
