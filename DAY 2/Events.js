3e618f55bca39091237c0baa2bfa82484c16bcca
SAHOOPRATISHRUTI <pratishrutisahoo7@gmail.com>
2023-11-11 13:17:21 +0530

    Intial Commit

---
+++ DAY 2/Events.js
@@ -0,0 +1,8 @@
+<><>
+    <button onclick="document.getElementById('demo').innerHTML = Date()">The time is?</button>
+
+    <button onclick="this.innerHTML = Date()">The time is?</button>
+
+    <button onclick="displayDate()">The time is?</button></>
+    
+    <button onclick="alert('Hello')">Click me.</button></>
